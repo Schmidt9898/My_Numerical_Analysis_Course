03bfc3462533cbca7f203359c99e485fd4fa39fa
SchmidtLaci (Warden) <schmidtlacus@gmail.com>
2023-04-18 12:41:01 +0200

    hw2 done

--- numanal2/boundary.py
+++ numanal2/boundary.py
@@ -10,7 +10,28 @@ class BoundaryCondition(Enum):
 
 
 def Errornorm( u_e , u_h ):
-	pass
+	u_h = np.asarray(u_h)
+	# ERRORNORM This function computes L2 error
+	# Computes an estimated distance between the
+	# exact solution and the numerical solution .
+	# The integral is approximated with three - point
+	# Gauss - Legendre quadrature rule
+	# u_e : Exact solution ( function )
+	# u_h : Numerical solution ( vector of nodal values )
+	n = len ( u_h ) # number of subintervals
+	h = 1.0 / ( n - 1) # length of subintervals
+	# rule for reference interval [0 , 1]
+	t = np.asarray([ (1 - math.sqrt (3/5)) / 2 , 1 / 2 , (1 + math.sqrt (3/5)) / 2 ])
+	w = np.asarray([ 5/18 , 8/18 , 5/18 ])
+	e2 = 0
+	for i  in range(0,n-1):
+		# evaluate at quadrature points
+		points = (i) * h + h * t
+		v_e = [u_e(x) for x in points]
+		v_h = (1 - t ) * u_h[i] + t * u_h[i+1]
+		# square difference and sum with weights
+		e2 += sum(np.power(v_e - v_h,2) * w)
+	return  math.sqrt ( h * abs( e2 ) )
 
 def trapazoid(a,b,h):
 	return (a+b) / (2*h) 
@@ -30,7 +51,7 @@ def LoadVector(N, f):
 
 def StiffnessMatrix(N, c):
 	h = 1 / (N-1)
-	print(h)
+	#print(h)
 	A = np.zeros((N,N))
 
 	A[0,0] = trapazoid(c[0],c[1],h)
@@ -71,48 +92,128 @@ def ApplyBoundary(A,b,b_left,b_right,h):
 	return A,b
 
 
-
-U_2_der = lambda x : -6*x # -u'' = 6x 
-U = lambda x : - math.pow(x,3) + 3*x # u = -x^3 + 3x hand calculated
-
-input_function = U_2_der 
-#input_function = lambda x : 1 
-
-
-
-N = 100
+N = 5
 X = np.linspace(0,1,N)
 h = 1 / (N-1)
 
+# -(cu')' = f
+
+print("Exercise 1")
+
+U_2_der = lambda x : 6*x # -u'' = 6x 
+U = lambda x : - math.pow(x,3) + 3*x # u = -x^3 + 3x
+
+f_function = U_2_der 
+c_function = lambda x : 1 
+
+
 solution = [U(x) for x in X]
-c = [input_function(x) for x in X]
+f = [f_function(x) for x in X]
+c = [c_function(x) for x in X]
 
 
-print(N)
-print(c)
-print(solution)
+print("N:",N)
+print("c:",c)
+print("f:",f)
+print("Exact solution:", np.asarray(solution))
 
 bound_a = (0,BoundaryCondition.Dirichlet)
 bound_b = (0,BoundaryCondition.Neumann)
 
 A_matrix = StiffnessMatrix(N, c)
-b_vector = LoadVector(N, c)
+b_vector = LoadVector(N, f)
 
 A_matrix, b_vector = ApplyBoundary(A_matrix,b_vector,bound_a,bound_b,h)
 
-print(A_matrix)
-print(b_vector)
+print("A_matrix:",A_matrix)
+print("b_vector:",b_vector)
 
 
 ans = np.linalg.solve(A_matrix, b_vector)
-print(np.allclose(np.dot(A_matrix, ans), b_vector))
+#print(np.allclose(np.dot(A_matrix, ans), b_vector))
 
-plt.figure("ans")
+
+print("u_h:",ans)
+print("Errornorm:",Errornorm(U,ans))
+
+
+
+plt.figure("Exercise 1")
+plt.plot(X,ans)
+plt.plot(X,solution)
+
+print("-----------------------------")
+
+Ns = [4,8,16,32,64]
+
+# building table:
+last_diff = None
+r = None
+for n in Ns:
+	n+=1 # because
+	XX = np.linspace(0,1,n)
+	h = 1 / (n-1)
+
+	solution = [U(x) for x in X]
+	f = [f_function(x) for x in XX]
+	c = [c_function(x) for x in XX]
+
+	A_matrix = StiffnessMatrix(n, c)
+	b_vector = LoadVector(n, f)
+	A_matrix, b_vector = ApplyBoundary(A_matrix,b_vector,bound_a,bound_b,h)
+	ans = np.linalg.solve(A_matrix, b_vector)
+	error = Errornorm(U,ans)
+	if last_diff is None:
+		last_diff = error
+	else:
+		r = math.log2(error/last_diff)*-1 ## hehe
+		last_diff = error
+	print("n-1:",n-1,"Errornorm:",error,"Rate:",r)
+print("-----------------------------")
+
+
+# Exercise 2
+print("Exercise 2")
+
+
+h = 1 / (N-1)
+f_function = lambda x : 4*x
+c_function = lambda x : 1+x 
+
+U = lambda x: 2*x - math.pow(x,2) + math.log(1+x) - math.log(2)
+
+solution = [U(x) for x in X]
+f = [f_function(x) for x in X]
+c = [c_function(x) for x in X]
+
+
+
+
+print("N:",N)
+print("c:",c)
+print("f:",f)
+print("Exact solution:", np.asarray(solution))
+
+bound_a = (3,BoundaryCondition.Neumann)
+bound_b = (1,BoundaryCondition.Dirichlet)
+
+A_matrix = StiffnessMatrix(N, c)
+b_vector = LoadVector(N, f)
+
+A_matrix, b_vector = ApplyBoundary(A_matrix,b_vector,bound_a,bound_b,h)
+
+print("A_matrix:",A_matrix)
+print("b_vector:",b_vector)
+
+
+ans = np.linalg.solve(A_matrix, b_vector)
+#print(np.allclose(np.dot(A_matrix, ans), b_vector))
+
+print("u_h:",ans)
+print("Errornorm:",Errornorm(U,ans))
+
+plt.figure("Exercise 2")
 plt.plot(X,ans)
 plt.plot(X,solution)
 
 plt.show()
-
-#a_val,type = bound_a
-#if type == "dir":
-#	h*c[-1]/2 + bound_a(0) 
